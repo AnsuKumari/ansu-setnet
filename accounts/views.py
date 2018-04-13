e9b9ad637f7147299a810bd8c1444b6cf8f30991
Ansu Kumari <ansu.kumari@outlook.com>
2018-04-13 12:34:22 +0530

    Make setnet.

---
+++ accounts/views.py
@@ -0,0 +1,44 @@
+from django.contrib.auth import login, authenticate
+from django.shortcuts import render, get_object_or_404,  redirect
+from .forms import RegisterForm, EditForm
+from django.contrib.auth.models import User
+from .models import Profile
+from upost.models import Post
+
+def register(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()	
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('post_list')
+    else:
+        form = RegisterForm()
+    return render(request, 'registration/register.html', {'form': form})
+
+def profile(request, pk=None):
+    # 	user = Profile.objects.filter(user=request.user.id)[0]
+    if pk:
+    	user = get_object_or_404(User, pk=pk)
+    else:
+    	user = request.user
+    posts = Post.objects.filter(user=user)
+    dp_url = user.profile.dp if user.profile.dp else 'images/dp/user-default-sm.png'
+    cp_url = user.profile.cp if user.profile.cp else 'images/cp/cover-default.jpg'
+    return render(request, 'registration/profile.html', {'profile_user': user, 'posts':posts, 'dp_url':dp_url, 'cp_url':cp_url})
+
+def edit_profile(request, pk):
+    profile = get_object_or_404(Profile, pk=pk)
+    if request.method == "POST":
+        form = EditForm(request.POST, request.FILES, instance=profile)
+        if form.is_valid():
+            profile = form.save(commit=False) #Table not changed yet
+            if profile.user == request.user:
+                profile.save() # since commit = True, changes saved to table
+            return redirect('profile')
+    else:
+        form = EditForm(instance=profile)
+    return render(request, 'registration/edit_profile.html', {'form': form})
