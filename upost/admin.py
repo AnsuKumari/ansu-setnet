7a063276f60d646fcfda8629ee5d697030e6f7b0
Ansu Kumari <ansu.kumari@outlook.com>
2018-04-14 11:04:29 +0530

    Register apps to admin with fields show function.

--- upost/admin.py
+++ upost/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 from .models import Post, Comment
 
-admin.site.register(Post)
-admin.site.register(Comment)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'text', 'created', 'modified')
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'post', 'user', 'text', 'created')
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Comment, CommentAdmin)
--- upost/models.py
+++ upost/models.py
@@ -4,7 +4,7 @@ from django.utils import timezone
 
 class Post(models.Model):
     user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
-    text = models.CharField(max_length=20) # replace charfield with models.TextField() to be able to receive larger posts.
+    text = models.CharField(max_length=500) # replace charfield with models.TextField() to be able to receive larger posts.
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
 
@@ -14,7 +14,7 @@ class Post(models.Model):
 class Comment(models.Model):
     post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
     user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
-    text = models.TextField()
+    text = models.CharField(max_length=5000)
     created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
